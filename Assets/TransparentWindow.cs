57e0b0c459ab6d92a39d6b7b94737e2682acea01
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-10 22:10:25 +0100

    Updated transparent code

--- Assets/TransparentWindow.cs
+++ Assets/TransparentWindow.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
-    [DllImport("user32.dll")]
-    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
-    
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetActiveWindow();
+    /* -------------------------------- Variables ------------------------------- */
+    const int GWL_EXSTYLE = -20;
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
 
     private struct MARGINS {
         public int cxLeftWidth;
@@ -16,16 +15,37 @@ public class TransparentWindow : MonoBehaviour
         public int cyTopHeigh;
         public int cyBottomHeight;
     }
+
+    /* ------------------------------ Unity methods ----------------------------- */
+    // START
+    private void Start() {
+        //MessageBox(new IntPtr(0), "Hello world!", "Test text", 0);
+
+        #if !UNITY_EDITOR
+            IntPtr hWnd = GetActiveWindow();
+            MARGINS margins = new MARGINS {cxLeftWidth = -1 };
+
+            DwmExtendFrameIntoClientArea(hWnd, ref margins);
+            SetwindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+        #endif
+            Application.runInBackground = true;
+
+    }
+
+    /* ----------------------------- Windows methods ---------------------------- */
+    
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetwindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
     
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
-    private void Start() {
-        //MessageBox(new IntPtr(0), "Hello world!", "Test text", 0);
-
-        IntPtr hWnd = GetActiveWindow();
-        MARGINS margins = new MARGINS {cxLeftWidth = -1 };
-        
-        DwmExtendFrameIntoClientArea(hWnd, ref margins);
-    }
+    
 }
