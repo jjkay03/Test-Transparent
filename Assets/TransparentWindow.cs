9e57d91294a6cd818f8980aa9a852f24e860b8f6
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-10 22:37:54 +0100

    Update TransparentWindow.cs

--- Assets/TransparentWindow.cs
+++ Assets/TransparentWindow.cs
@@ -4,48 +4,45 @@ using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
-    /* -------------------------------- Variables ------------------------------- */
     const int GWL_EXSTYLE = -20;
-    const uint WS_EX_LAYERED = 0x00080000;
-    const uint WS_EX_TRANSPARENT = 0x00000020;
+    const int WS_EX_LAYERED = 0x80000;
+    const int WS_EX_TRANSPARENT = 0x20;
 
-    private struct MARGINS {
+    [StructLayout(LayoutKind.Sequential)]
+    struct MARGINS
+    {
         public int cxLeftWidth;
         public int cxRightWidth;
-        public int cyTopHeigh;
+        public int cyTopHeight;
         public int cyBottomHeight;
     }
 
-    /* ------------------------------ Unity methods ----------------------------- */
-    // START
-    private void Start() {
-        //MessageBox(new IntPtr(0), "Hello world!", "Test text", 0);
-
-        #if !UNITY_EDITOR
-            IntPtr hWnd = GetActiveWindow();
-            MARGINS margins = new MARGINS {cxLeftWidth = -1 };
-
-            DwmExtendFrameIntoClientArea(hWnd, ref margins);
-            SetwindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
-
-        #endif
-            Application.runInBackground = true;
-
-    }
-
-    /* ----------------------------- Windows methods ---------------------------- */
-    
     [DllImport("user32.dll")]
     public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
-    
-    [DllImport("user32.dll")]
+
+    [DllImport("user32.dll", SetLastError = true)]
     private static extern IntPtr GetActiveWindow();
 
-    [DllImport("user32.dll")]
-    private static extern int SetwindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-    
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
-    
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    private void Start()
+    {
+        // Don't run code if in unity editor
+        #if !UNITY_EDITOR
+            IntPtr hWnd = GetActiveWindow();
+            MARGINS margins = new MARGINS { cxLeftWidth = -1 };
+
+            // Extend the frame into the client area to include the transparent parts
+            DwmExtendFrameIntoClientArea(hWnd, ref margins);
+
+            // Make the window layered and transparent
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        
+        #endif
+        Application.runInBackground = true;
+    }
 }
