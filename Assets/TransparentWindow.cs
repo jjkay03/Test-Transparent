6d2c530e5af7932c4d800a5c12d28496de4173f0
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-10 21:47:15 +0100

    Transparent window first

--- Assets/TransparentWindow.cs
+++ Assets/TransparentWindow.cs
@@ -6,8 +6,26 @@ public class TransparentWindow : MonoBehaviour
 {
     [DllImport("user32.dll")]
     public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    private struct MARGINS {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeigh;
+        public int cyBottomHeight;
+    }
+    
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
     private void Start() {
-        MessageBox(new IntPtr(0), "Hello world!", "Test text", 0);
+        //MessageBox(new IntPtr(0), "Hello world!", "Test text", 0);
+
+        IntPtr hWnd = GetActiveWindow();
+        MARGINS margins = new MARGINS {cxLeftWidth = -1 };
+        
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
     }
 }
