6d1f64edf8ab60083ad56739c1894aad39ca080e
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-10 23:14:46 +0100

    Updated transparent code

--- Assets/TransparentWindow.cs
+++ Assets/TransparentWindow.cs
@@ -1,21 +1,31 @@
 using System;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
+    /* -------------------------------- Variables ------------------------------- */
     const int GWL_EXSTYLE = -20;
     const int WS_EX_LAYERED = 0x80000;
     const int WS_EX_TRANSPARENT = 0x20;
+    const int LWA_COLORKEY = 0x00000001;
 
     [StructLayout(LayoutKind.Sequential)]
-    struct MARGINS
-    {
+    struct MARGINS {
         public int cxLeftWidth;
         public int cxRightWidth;
         public int cyTopHeight;
         public int cyBottomHeight;
     }
+
+    [Header("Transparent Window Settings")]
+    public bool stayOnTop = true;
+
+
+    /* -------------------------- Windows Lib Functions ------------------------- */
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
     [DllImport("user32.dll")]
     public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
@@ -23,14 +33,15 @@ public class TransparentWindow : MonoBehaviour
     [DllImport("user32.dll", SetLastError = true)]
     private static extern IntPtr GetActiveWindow();
 
-    [DllImport("Dwmapi.dll")]
-    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
-
     [DllImport("user32.dll", SetLastError = true)]
     private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-    private void Start()
-    {
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+
+    /* --------------------------------- Methods -------------------------------- */
+    private void Start() {
         // Don't run code if in unity editor
         #if !UNITY_EDITOR
             IntPtr hWnd = GetActiveWindow();
@@ -41,8 +52,13 @@ public class TransparentWindow : MonoBehaviour
 
             // Make the window layered and transparent
             SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+            // Make the window stay on top
+            if (stayOnTop == true) { SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0); }
+            
         
         #endif
-        Application.runInBackground = true;
+            // Run application when its in the background (not selected)
+            Application.runInBackground = true;
     }
 }
